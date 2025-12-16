447ed01f8ab8f87faa2f85b08dbc853368b2ccfc
fyonecon <fyonecon@gmail.com>
2025-12-17 03:28:43 +0800

    update

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -4,10 +4,11 @@ const config = {
     app: {
         app_name: "Waigo",
         app_class: "waigo_sv_", // ginthon_sv_ 、waigo_sv_
-        app_version: "1.4.0", // 1.0.0
+        app_version: "1.5.0", // 1.0.0
     },
     sys:{
         backend: "go", // go、py
+        home_route: "/home" // 主页默认页的路由 “”、"/home”
     },
     api: {
         js_call_go_url: "http://127.0.0.1:9850/", // http://127.0.0.1:9750/ 、http://127.0.0.1:9850/

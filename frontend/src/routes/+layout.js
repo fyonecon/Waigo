08d048dbae605a98b8a78598462ea01c25448822
fyonecon <fyonecon@gmail.com>
2026-01-04 20:39:24 +0800

    增加ssr策略

--- frontend/src/routes/+layout.js
+++ frontend/src/routes/+layout.js
@@ -1,2 +1,7 @@
-export const ssr = false; // false关闭服务端渲染
-export const csr = true; // 确保客户端渲染
+// 全局
+
+import config from "../config.js";
+
+export const csr = config.sys.csr; // 确保客户端渲染
+export const ssr = config.sys.ssr; // false关闭服务端渲染，false关闭SEO。（false可以让js正确访问“window”对象）
+
--- frontend/src/routes/settings/about/+page.js
+++ frontend/src/routes/settings/about/+page.js
@@ -1,9 +1,13 @@
-import { dev } from '$app/environment';
+import {dev} from '$app/environment';
+import config from "../../../config.js";
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
-export const csr = dev;
+export const csr = config.sys.csr; // dev页面无js、true页面有js
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+//
+export const ssr = config.sys.ssr;  // 单独设置SEO，true开启 服务端渲染+SEO

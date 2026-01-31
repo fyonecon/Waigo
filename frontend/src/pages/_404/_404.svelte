70b84e111bdb0818893df5854ed3dadde12c91ab
fyonecon <fyonecon@gmail.com>
2026-01-31 18:16:19 +0800

    update

--- frontend/src/pages/_404/_404.svelte
+++ frontend/src/pages/_404/_404.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { resolve } from '$app/paths';
-    import func from "../../common/func.svelte";
+    import func from "../../common/func.svelte.js";
     import config from "../../config";
     import {afterNavigate} from "$app/navigation";
     import {onMount} from "svelte";
@@ -60,8 +60,10 @@
     <br/>
     <br/>
     <div class="" style="display:flex; justify-content: center; align-items: center;">
-        <div>
-            <a class="click font-blue" href={resolve(back_url)} target="_self" title="Back Home"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="12" cy="12" r="10"/><path stroke-linecap="round" stroke-linejoin="round" d="m15.5 9l-3 3l3 3m-4-6l-3 3l3 3"/></g></svg> {func.get_translate("a_click_tip_back_home")}</a>
+        <div style="clear: both;">
+            <a class="click font-blue" href={resolve(back_url)} target="_self" title="Back Home"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="float: left; margin-right: 10px;"><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="12" cy="12" r="10"/><path stroke-linecap="round" stroke-linejoin="round" d="m15.5 9l-3 3l3 3m-4-6l-3 3l3 3"/></g></svg>{func.get_translate("a_click_tip_back_home")}</a>
         </div>
     </div>
+    <br/>
+    <br/>
 </div>
